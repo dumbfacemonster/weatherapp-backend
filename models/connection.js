a9b140cc7ba5f68e55917f3534a36a89db589a71
Sarah <sarah.saker@hotmail.fr>
2024-06-29 18:01:02 +0200

    Modifs inclusions variables env

--- models/connection.js
+++ models/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const connectionString = 'mongodb+srv://admin:rjxJtmeEDXlbTaum@cluster0.eopwsxv.mongodb.net/weatherapp';
+const connectionString = process.env.CONNECTION_STRING;
 
 mongoose.connect(connectionString, { connectTimeoutMS: 2000 })
   .then(() => console.log('Database connected'))
